9d3a14fb4477909cb7a9606fcff7cee126592e48
kkomarkova <katerina.komarkova@hotmail.com>
2020-10-15 00:53:11 +0200

    Assigment2

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,44 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
+let inputText = <HTMLInputElement> document.getElementById("input");
+let selectedOption = <HTMLSelectElement> document.getElementById("letters");
+let result:HTMLElement = document.getElementById("table");
+let itemId:number = 1;
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+document.getElementById("doit").addEventListener("click", AddElement);
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function AddElement(){
+    //Creates the table row element
+    let trElement:HTMLTableRowElement = document.createElement("tr");
+    result.appendChild(trElement);
+
+    //Adds the number to the row
+    let itemNumber:HTMLTableHeaderCellElement = document.createElement("th");
+    itemNumber.innerHTML = itemId.toString();
+    trElement.appendChild(itemNumber);
+
+    //Adds the text to the row
+    let itemText:HTMLTableHeaderCellElement = document.createElement("th");
+    let text:string;
+    if(selectedOption.value == "upper"){
+        text = inputText.value.toUpperCase();
+    }
+    else{
+        text = inputText.value.toLowerCase();
+    }
+    itemText.innerHTML = text;
+    trElement.appendChild(itemText);
+
+    //Adds a class based on the number of the item
+    if(itemId%2 == 0){
+        trElement.className = "even";
+    }
+    else{
+        trElement.className = "uneven";
+    }
+
+    //Gives each created element its own unique id
+    trElement.id = itemId.toString();
+    itemNumber.id = "number" + itemId.toString();
+    itemText.id = "text" + itemId.toString();
+
+    itemId++;
+}
