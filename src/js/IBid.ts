4d4116ecf8016f8e2aacdf453c0c1075cfaa8fa5
kkomarkova <katerina.komarkova@hotmail.com>
2020-10-30 13:08:48 +0100

    changes

---
+++ src/js/IBid.ts
@@ -0,0 +1,6 @@
+export interface IBid{
+    id : number;
+    item: string;
+    bid: number;
+    name:string;
+    }
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,44 +1,134 @@
-let inputText = <HTMLInputElement> document.getElementById("input");
-let selectedOption = <HTMLSelectElement> document.getElementById("letters");
-let result:HTMLElement = document.getElementById("table");
-let itemId:number = 1;
+import axios,{
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+import { IBid } from "./Ibid";
 
-document.getElementById("doit").addEventListener("click", AddElement);
+ //url for the rest webservice at Azure
+ let bidWebUrl: string = "https://restcoinservice20201030093309.azurewebsites.net/api/bids/";
+//Get all bids eventlistener
+ let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("bidsContent");
+ let GetAllBidsButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("getAllButton");
 
-function AddElement(){
-    //Creates the table row element
-    let trElement:HTMLTableRowElement = document.createElement("tr");
-    result.appendChild(trElement);
+ GetAllBidsButton.addEventListener('click',showAllBids);
+ //Get one bids by id eventlistener
+ let getOneBidButton: HTMLButtonElement =<HTMLButtonElement> document.getElementById("getOneButton");
+getOneBidButton.addEventListener("click", addoneBid)
+//Bidlist
+let getOneBidList : HTMLOListElement = <HTMLOListElement> document.getElementById("getoneBidlist");
+//Add Bid
+let AddBidButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("addButton");
+AddBidButton.addEventListener('click',addBid);
+//Delete car button eventlistener
+let deleteBidButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("deleteButton");
+deleteBidButton.addEventListener('click',deleteBid);
 
-    //Adds the number to the row
-    let itemNumber:HTMLTableHeaderCellElement = document.createElement("th");
-    itemNumber.innerHTML = itemId.toString();
-    trElement.appendChild(itemNumber);
+function deleteBid():void{
 
-    //Adds the text to the row
-    let itemText:HTMLTableHeaderCellElement = document.createElement("th");
-    let text:string;
-    if(selectedOption.value == "upper"){
-        text = inputText.value.toUpperCase();
-    }
-    else{
-        text = inputText.value.toLowerCase();
-    }
-    itemText.innerHTML = text;
-    trElement.appendChild(itemText);
+    //finds the id for the car to delete
+    let delBidIdElement: HTMLInputElement = <HTMLInputElement> document.getElementById("deleteBidId");
+    let myBidId : number = +delBidIdElement.value;
+    
+    let deleteContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("deletecontent");
 
-    //Adds a class based on the number of the item
-    if(itemId%2 == 0){
-        trElement.className = "even";
-    }
-    else{
-        trElement.className = "uneven";
-    }
+    
+    //http delete request with one parameter(params) id that is set to myCarId
+    axios.delete("https://restcoinservice20201030093309.azurewebsites.net/api/bids/"+myBidId
+        )
+        .then((response :  AxiosResponse): void => {
+                console.log("Bid is deleted ");
+                console.log("Statuscode is :" + response.status);
+                deleteContentElement.innerHTML = "bid is deleted";
+        })
+        .catch(
+            (error:AxiosError) : void => {                          
+                console.log(error);
+                deleteContentElement.innerHTML = "Error: the bid is NOT deleted, look at the console";
+            });
 
-    //Gives each created element its own unique id
-    trElement.id = itemId.toString();
-    itemNumber.id = "number" + itemId.toString();
-    itemText.id = "text" + itemId.toString();
-
-    itemId++;
+            //https://www.typescriptlang.org/docs/handbook/functions.html
+            //se arrow functions 
 }
+
+function addBid():void{
+    let addItemelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addItem");
+    let addNameelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addName");
+    let addPriceelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addPrice");
+
+    let myItem : string = addItemelement.value;
+    let myName: string = addNameelement.value;
+    let myPrice : number = +addPriceelement.value;  
+
+    axios.post<IBid>("https://restcoinservice20201030093309.azurewebsites.net/api/bids/",
+                    {item:myItem,name:myName,price:myPrice})
+                    .then(function (response :  AxiosResponse): void
+                    {
+                        console.log("Statuscode is :" + response.status);
+                    })
+                    .catch(
+                        function (error:AxiosError) : void{                          
+                            console.log(error);
+                        }
+                    )
+                    
+
+}
+
+  //Function Get 1 Bid by id 
+  function addoneBid():void{
+    let addoneBidelement : HTMLInputElement = <HTMLInputElement> document.getElementById("getOne")
+    let myBidId : number = +addoneBidelement.value;
+    console.log("In the addoneBid function");
+    console.log("My card ID" + myBidId)
+    axios.get(bidWebUrl +"/" + myBidId)
+    .then (function(response:AxiosResponse<IBid>):void{
+        console.log(response);
+        console.log("Status is : " + response.status);
+        let myBid: IBid = response.data;
+        let newNode: HTMLLIElement = AddLiElement ("Name" + myBid.name );
+        getOneBidList.appendChild(newNode);
+    })
+} 
+ //Function showallBids - GET all bids from list
+ function showAllBids():void{
+
+    axios.get<IBid[]>("https://restcoinservice20201030093309.azurewebsites.net/api/bids/")
+    .then(function (response: AxiosResponse<IBid[]>) : void
+    {
+        console.log("are in then");
+        console.log(response);
+
+        // let result: string = "<ol>" 
+        //remove all the li elements one by one
+    while (ContentElement.firstChild) {
+        ContentElement.removeChild(ContentElement.lastChild);
+    }
+   
+
+
+        response.data.forEach((bid: IBid) => {
+            let newNode:HTMLLIElement = AddLiElement(bid.id + " " +bid.item+ " "+bid.name + "" + bid.bid);
+            ContentElement.appendChild(newNode);
+        
+        });
+         
+    })
+    .catch(
+        function (error:AxiosError) : void{
+            console.log("Error in the typescript code");
+            console.log(error);
+
+        }
+    )
+    
+    console.log("At the end in the showAllBids function");
+
+ }
+ function AddLiElement(text:string):HTMLLIElement {
+    let newLi:HTMLLIElement = document.createElement('li');
+    let newTextNode:Text = document.createTextNode(text)
+    newLi.appendChild(newTextNode);
+            // list.appendChild(newLi);
+    return newLi;
+ }
+
